4de699b42ce54eba64e695b9f70a20f4937aa327
IvayloPaskalev <ivaylo.paskalew@gmail.com>
2016-09-15 19:36:25 +0300

    Added task-1 and task-2 solutions

---
+++ 01.Promises and Asynchronous Programming/02. Popup then load/task-2.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Ivaylo on 15.9.2016 г..
+ */
+(function () {
+    let promise = new Promise((resolve, reject) => {
+        resolve();
+    });
+
+    function popup() {
+        window.confirm('Click OK !');
+    }
+
+    function waitTwoSeconds() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }
+
+    function redirectToWebsite() {
+        document.location = 'https://telerikacademy.com/';
+    }
+
+    promise
+        .then(popup)
+        .then(waitTwoSeconds)
+        .then(redirectToWebsite);
+}());
